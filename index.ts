35bb586c55181bfe4c7da3a12745fc98fd3adff1
berger <robertberger.dev@gmail.com>
2023-01-26 13:25:38 -0500

    ran npm install --save-dev @types/validator

--- index.ts
+++ index.ts
@@ -1,10 +1,27 @@
-import Contact, { zip } from './contact';
-import * as values from './values';
-import NumberUtilities from './numberExample';
+// import Contact, { zip } from './contact';
+// import * as values from './values';
+// import NumberUtilities from './numberExample';
 
-let newContact = new Contact(values.firstName, values.lastName);
-console.log(newContact.fullName());
-console.log(zip);
+// let newContact = new Contact(values.firstName, values.lastName);
+// console.log(newContact.fullName());
+// console.log(zip);
 
-let result: string = NumberUtilities.isOddOrEven(51);
-console.log(result);
+// let result: string = NumberUtilities.isOddOrEven(51);
+// console.log(result);
+
+import validator from 'validator';
+
+let email = "test@test.com";
+let notEmail = "test.com";
+
+if (validator.isEmail(email)) {
+    console.log("email is valid")
+} else {
+    console.log("email is not valid")
+}
+
+if (validator.isEmail(notEmail)) {
+    console.log("notEmail is valid")
+} else {
+    console.log("notEmail is not valid")
+}
