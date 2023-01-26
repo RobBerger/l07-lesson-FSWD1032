35c965ee997ffdc66f181e1386e236db678e213e
berger <robertberger.dev@gmail.com>
2023-01-26 11:51:29 -0500

    created contact.ts file and added export class contact

---
+++ contact.ts
@@ -0,0 +1,13 @@
+export class Contact {
+    firstName: string;
+    lastName: string
+
+    constructor(first: string, last: string) {
+        this.firstName = first;
+        this.lastName = last;
+    }
+
+    fullName() {
+        return this.firstName + " " + this.lastName;
+    }
+}
