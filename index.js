b52fc8c99cd3e510546bfe684d5f783f01674f44
berger <robertberger.dev@gmail.com>
2023-01-26 12:06:26 -0500

    ran npm start

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const contact_1 = require("./contact");
+const values_1 = require("./values");
+let newContact = new contact_1.Contact(values_1.firstName, values_1.lastName);
+console.log(newContact.fullName());
