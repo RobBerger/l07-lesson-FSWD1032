7dd9ab74f5aa01ae8c52c2817a7d7f3b40dc03e7
berger <robertberger.dev@gmail.com>
2023-01-26 12:49:07 -0500

    ran npm start

--- index.js
+++ index.js
@@ -22,9 +22,15 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const contact_1 = __importStar(require("./contact"));
 const values = __importStar(require("./values"));
+const numberExample_1 = __importDefault(require("./numberExample"));
 let newContact = new contact_1.default(values.firstName, values.lastName);
 console.log(newContact.fullName());
 console.log(contact_1.zip);
+let result = numberExample_1.default.isOddOrEven(51);
+console.log(result);
