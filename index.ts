ffaf9244f413dded48d8264c275558c297eee4f3
berger <robertberger.dev@gmail.com>
2023-01-26 12:47:55 -0500

    added function to index.ts

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import Contact, { zip } from './contact';
 import * as values from './values';
+import NumberUtilities from './numberExample';
 
 let newContact = new Contact(values.firstName, values.lastName);
 console.log(newContact.fullName());
