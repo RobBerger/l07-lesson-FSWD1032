4b59e5289aaded4b3e28f1c71a03135ddd57b261
berger <robertberger.dev@gmail.com>
2023-01-26 13:26:25 -0500

    ran npm start on new example code

--- index.js
+++ index.js
@@ -1,36 +1,28 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
+// import Contact, { zip } from './contact';
+// import * as values from './values';
+// import NumberUtilities from './numberExample';
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const contact_1 = __importStar(require("./contact"));
-const values = __importStar(require("./values"));
-const numberExample_1 = __importDefault(require("./numberExample"));
-let newContact = new contact_1.default(values.firstName, values.lastName);
-console.log(newContact.fullName());
-console.log(contact_1.zip);
-let result = numberExample_1.default.isOddOrEven(51);
-console.log(result);
+// let newContact = new Contact(values.firstName, values.lastName);
+// console.log(newContact.fullName());
+// console.log(zip);
+// let result: string = NumberUtilities.isOddOrEven(51);
+// console.log(result);
+const validator_1 = __importDefault(require("validator"));
+let email = "test@test.com";
+let notEmail = "test.com";
+if (validator_1.default.isEmail(email)) {
+    console.log("email is valid");
+}
+else {
+    console.log("email is not valid");
+}
+if (validator_1.default.isEmail(notEmail)) {
+    console.log("notEmail is valid");
+}
+else {
+    console.log("notEmail is not valid");
+}
