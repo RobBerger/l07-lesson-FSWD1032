e71c6d56081a575c3dd4b67ba00b5550daca802f
berger <robertberger.dev@gmail.com>
2023-01-26 12:46:06 -0500

    added export function

--- numberExample.ts
+++ numberExample.ts
@@ -1,5 +1,19 @@
 namespace numberExample {
+    function isNumber(value: any): boolean {
+        return !isNaN(value);
+    };
 
+    export function isOddOrEven(value: any): string {
+        if (!isNumber(value)) {
+            return "Not a Number";
+        }
+        if (value % 2 === 0) {
+            return "Even Number";
+        }
+        else {
+            return "Odd Number";
+        }
+    };
 }
 
 export default numberExample;
